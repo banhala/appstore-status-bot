d8393ab27216070fb7a986f108e885e4c79a0b77
JunhyeonOh <Oh.junhyeon95@gmail.com>
2023-09-04 14:00:36 +0900

    Create update_readme.py

---
+++ .github/update_readme.py
@@ -0,0 +1,13 @@
+# README.md 파일 열기
+with open("README.md", "r") as f:
+    lines = f.readlines()
+
+# "update date: " 라인 찾기
+for i, line in enumerate(lines):
+    if "update date: " in line:
+        # 날짜만 표시
+        lines[i] = f"update date: ({datetime.now().strftime('%Y-%m-%d')})\n"
+
+# README.md 파일 저장
+with open("README.md", "w") as f:
+    f.writelines(lines)
